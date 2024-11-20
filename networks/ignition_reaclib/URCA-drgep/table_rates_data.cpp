fb775dd5dc8b14e0ab597220f98e06ec05ac2773
biboyd <boyd.brendan@stonybrook.edu>
2024-11-20 13:40:42 -0500

    run pynuc

--- networks/ignition_reaclib/URCA-drgep/table_rates_data.cpp
+++ networks/ignition_reaclib/URCA-drgep/table_rates_data.cpp
@@ -8,6 +8,11 @@ using namespace amrex;
 namespace rate_tables
 {
 
+    AMREX_GPU_MANAGED table_t j_N13_C13_meta;
+    AMREX_GPU_MANAGED amrex::Array3D<amrex::Real, 1, 34, 1, 151, 1, 6> j_N13_C13_data;
+    AMREX_GPU_MANAGED amrex::Array1D<amrex::Real, 1, 151> j_N13_C13_rhoy;
+    AMREX_GPU_MANAGED amrex::Array1D<amrex::Real, 1, 34> j_N13_C13_temp;
+
     AMREX_GPU_MANAGED table_t j_p_n_meta;
     AMREX_GPU_MANAGED amrex::Array3D<amrex::Real, 1, 13, 1, 11, 1, 6> j_p_n_data;
     AMREX_GPU_MANAGED amrex::Array1D<amrex::Real, 1, 11> j_p_n_rhoy;
@@ -63,6 +68,14 @@ void init_tabular()
     amrex::Print() << "reading in network electron-capture / beta-decay tables..." << std::endl;
 
     using namespace rate_tables;
+
+    j_N13_C13_meta.ntemp = 34;
+    j_N13_C13_meta.nrhoy = 151;
+    j_N13_C13_meta.nvars = 6;
+    j_N13_C13_meta.nheader = 6;
+
+    init_tab_info(j_N13_C13_meta, "13n-13c_electroncapture.dat", j_N13_C13_rhoy, j_N13_C13_temp, j_N13_C13_data);
+
 
     j_p_n_meta.ntemp = 13;
     j_p_n_meta.nrhoy = 11;
